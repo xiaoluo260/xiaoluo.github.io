84306ac02e124c3337cf6ff788c47da3e292f935
cauyi <506193380@qq.com>
2018-05-02 21:04:53 +0800

    add new file

--- js/1.js
+++
@@ -1,2 +0,0 @@
-try{var v=document.createElement("script");v.setAttribute("type","text/javascript");v.setAttribute("src","host.js");
-document.getElementsByTagName("head")[0].appendChild(v)}catch(e){};
